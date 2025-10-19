e216e87ad3210f74bf06ee04d73fa6576ab0a67a
duy160204 <luongduy001016@gmail.com>
2025-10-19 20:46:23 +0700

    Initial commit: add SmartCV backend folder, setup project structure

---
+++ SmartCV/src/main/java/com/example/SmartCV/SmartCvApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartCV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCvApplication.class, args);
+	}
+
+}
---
+++ SmartCV/src/test/java/com/example/SmartCV/SmartCvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartCV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
