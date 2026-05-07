59bc678bd05e44f01485a072e57f033adec6a325
duy160204 <luongduy001016@gmail.com>
2026-05-07 09:48:07 +0700

    edit db for deploy v3

--- SmartCV/src/main/resources/db/migration/V3__make_password_nullable.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Make password column nullable for OAuth users
--- Date: 2026-01-20
--- Purpose: Fix constraint mismatch - OAuth users are created without passwords
-
-ALTER TABLE users MODIFY COLUMN password VARCHAR(255) NULL;
